2c1938a48777248ae90b0f9dbfa282e127efbc4e
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-20 15:40:53 +0300

    axios get api

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Header = () => {
+  const [data, setData] = useState(null);
+
+  const getApi = () => {
+    axios
+      .get("https://randomuser.me/api/")
+      .then((res) => {
+        console.log(res);
+        setData(res.data.results[0]);
+      })
+      .catch((err) => console.log(err));
+  };
+  //   getApi();
+  return <div className=""></div>;
+};
+
+export default Header;
